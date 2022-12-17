b36ecba0c1bfe58e747d29c8689552bf2774212b
SKRIPPII <93274805+SKRIPPII@users.noreply.github.com>
2022-12-17 22:04:15 +0300

    Add files via upload

---
+++ MyLibrary/MyLibrary/Form.cs
@@ -0,0 +1,16 @@
+﻿namespace MyLibrary
+{
+    public abstract class Form
+    {
+        private double area;
+        public double GetArea => area;
+        public Form(double area)
+        {
+            this.area = area;
+        }
+        public override string ToString()
+        {
+            return GetArea.ToString();
+        }
+    }
+}
